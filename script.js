17ff07c8a9f233503e3669b5d971ac38baa10ea6
Angels512 <miguelcolmatiz@gmail.com>
2021-10-06 09:31:54 -0500

    Challenge finalizado - 06/Oct

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+// VARIABLES GLOBALES
+const socialMediaUI = document.querySelectorAll('.socialmediaicons');
+const socialName = ['facebook', 'flickr', 'google', 'linkedin', 'meetup', 'twitter', 'wordpress', 'youtube'];
+
 const socialMedia =
 {
   facebook: 'http://facebook.com',
@@ -9,3 +14,28 @@ const socialMedia =
   wordpress: 'http://wordpress.com',
   youtube: 'http://youtube.com'
 };
+
+
+// FUNCIONES
+const addImages = (navSocialMedia, socialName, socialMedia) =>
+{
+  let navSocial = navSocialMedia;
+
+  for (let i = 0; i < socialName.length; i++)
+  {
+    let url = document.createElement('a');
+    url.href = socialMedia[socialName[i]];
+    url.target = '_blank';
+
+    let image = document.createElement('img');
+    image.src = `images/${socialName[i]}.png`;
+    image.alt = socialName[i];
+
+    url.appendChild(image); // Agrego dentro del enlace la imagen
+    navSocial.appendChild(url); // Paso el elemento <a> a la vista con la imagen
+  }
+}
+
+
+// EVENTOS
+socialMediaUI.forEach((navSocialMedia) => { addImages(navSocialMedia, socialName, socialMedia); });
