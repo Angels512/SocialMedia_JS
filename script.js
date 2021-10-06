c763a7960e1e4374cbb78c0af0ef95574666079f
Angels512 <miguelcolmatiz@gmail.com>
2021-10-06 09:29:50 -0500

    Primer Commit SocialMedia - 06/Oct

---
+++ script.js
@@ -0,0 +1,11 @@
+const socialMedia =
+{
+  facebook: 'http://facebook.com',
+  flickr: 'http://flickr.com',
+  google: 'http://google.com',
+  linkedin: 'http://linkedin.com',
+  meetup: 'http://meetup.com',
+  twitter: 'http://twitter.com',
+  wordpress: 'http://wordpress.com',
+  youtube: 'http://youtube.com'
+};
